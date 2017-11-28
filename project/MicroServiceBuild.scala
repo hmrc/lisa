6b3dca2717930063e25e29cb102b652d0bee45ca
Chris Waugh <mail@chriswaugh.net>
2017-11-28 10:50:54 +0000

    Bumped version of auth-client

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -14,7 +14,7 @@ object MicroServiceBuild extends Build with MicroService {
   val compile = Seq(
     ws,
     "uk.gov.hmrc" %% "microservice-bootstrap" % "6.13.0",
-    "uk.gov.hmrc" %% "auth-client" % "2.3.0",
+    "uk.gov.hmrc" %% "auth-client" % "2.4.0",
     "uk.gov.hmrc" %% "domain" % "5.0.0"
   )
 
