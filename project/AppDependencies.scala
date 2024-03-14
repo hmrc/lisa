b69f2dee019fe97a166b83507fcb934ed265549b
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2024-03-14 14:22:06 +0000

    DDCE-5158: Play 3.0 migration (#49)
    * DDCE-5158: Play 3.0 migration
    
    * DDCE-5158: PR bot feedback resolved
    
    * DDCE-5158: PR reviewer feedback resolved

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,20 +4,20 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "7.21.0"
+  val bootstrapPlayVersion = "8.5.0"
 
  private val compile = Seq(
     ws,
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
-    "uk.gov.hmrc"             %% "domain"                    % "8.3.0-play-28"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
+    "uk.gov.hmrc"             %% "domain-play-30"            % "9.0.0"
    )
 
     private val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"    % bootstrapPlayVersion,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"    % bootstrapPlayVersion,
     "org.pegdown"             %  "pegdown"                   % "1.6.0",
-    "com.typesafe.play"       %% "play-test"                 % PlayVersion.current,
-    "org.scalatest"           %% "scalatest"                 % "3.2.16",
-    "org.scalatestplus.play"  %% "scalatestplus-play"        % "5.1.0",
+    "org.playframework"       %% "play-test"                 % PlayVersion.current,
+    "org.scalatest"           %% "scalatest"                 % "3.2.18",
+    "org.scalatestplus.play"  %% "scalatestplus-play"        % "7.0.1",
     "org.scalatestplus"       %% "mockito-3-4"               % "3.2.10.0",
     "com.vladsch.flexmark"    % "flexmark-all"               % "0.64.8"
   ).map(_ % Test)
