b8168eb539fe4a1c36d946eac6d768686a9e3f6f
harish-korada1 <174133905+harish-korada1@users.noreply.github.com>
2025-08-18 15:58:59 +0100

    DDCE-7006: addressing the comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,11 +12,7 @@ object AppDependencies {
    )
 
     private val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"    % bootstrapPlayVersion,
-    "org.scalatest"           %% "scalatest"                 % "3.2.19",
-    "org.scalatestplus.play"  %% "scalatestplus-play"        % "7.0.2",
-    "org.scalatestplus"       %% "mockito-3-4"               % "3.2.10.0",
-    "com.vladsch.flexmark"    % "flexmark-all"               % "0.64.8"
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"    % bootstrapPlayVersion
   ).map(_ % Test)
 
  def apply(): Seq[ModuleID] = compile ++ test
