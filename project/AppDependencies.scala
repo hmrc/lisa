9008a4411694111a7dac0c1ac12caa140b544e24
Harish Korada <174133905+harish-korada1@users.noreply.github.com>
2025-08-19 10:19:11 +0100

    DDCE-7006: [LISA]: Fix Jackson Core Vulnerability [lisa] (#53)
    * DDCE-7006: [LISA]: Fix Jackson Core Vulnerability [lisa]
    
    * DDCE-7006: fixing the PR comments
    
    * DDCE-7006: adding the coverage
    
    * DDCE-7006: addressing the comments
    
    * DDCE-7006: addressing the comments
    
    * DDCE-7006: addressing the comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,25 +1,15 @@
-import play.core.PlayVersion
-import play.sbt.PlayImport.ws
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "9.13.0"
+  val bootstrapPlayVersion = "9.19.0"
 
  private val compile = Seq(
-    ws,
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
-    "uk.gov.hmrc"             %% "domain-play-30"            % "11.0.0"
    )
 
     private val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"    % bootstrapPlayVersion,
-    "org.pegdown"             %  "pegdown"                   % "1.6.0",
-    "org.playframework"       %% "play-test"                 % PlayVersion.current,
-    "org.scalatest"           %% "scalatest"                 % "3.2.19",
-    "org.scalatestplus.play"  %% "scalatestplus-play"        % "7.0.1",
-    "org.scalatestplus"       %% "mockito-3-4"               % "3.2.10.0",
-    "com.vladsch.flexmark"    % "flexmark-all"               % "0.64.8"
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"    % bootstrapPlayVersion
   ).map(_ % Test)
 
  def apply(): Seq[ModuleID] = compile ++ test
